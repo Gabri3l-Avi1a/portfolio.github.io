5cac562f809820c66533e024ceea783824ff304a
Gabriel Avila <158302206+Gabri3l-Avi1a@users.noreply.github.com>
2026-04-01 10:03:07 +0200

    Update movement.js

--- js/movement.js
+++ js/movement.js
@@ -1,6 +1,6 @@
 const movableImage = document.getElementById('movableImage');
 const keys = {};
-
+const speed= 8
 document.addEventListener('keydown', (event) => {
     keys[event.key.toLowerCase()] = true;
 });
@@ -11,16 +11,16 @@ document.addEventListener('keyup', (event) => {
 
 function moveImage() {
     if (keys['w']) {
-        movableImage.style.top = `${movableImage.offsetTop - 10}px`;
+        movableImage.style.top = `${movableImage.offsetTop - speed}px`;
     }
     if (keys['s']) {
-        movableImage.style.top = `${movableImage.offsetTop + 10}px`;
+        movableImage.style.top = `${movableImage.offsetTop + speed}px`;
     }
     if (keys['a']) {
-        movableImage.style.left = `${movableImage.offsetLeft - 10}px`;
+        movableImage.style.left = `${movableImage.offsetLeft - speed}px`;
     }
     if (keys['d']) {
-        movableImage.style.left = `${movableImage.offsetLeft + 10}px`;
+        movableImage.style.left = `${movableImage.offsetLeft + speed}px`;
     }
     requestAnimationFrame(moveImage);
 }
