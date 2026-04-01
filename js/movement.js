809711815513bc8af66a3d5feb55bd8e537d2fb0
Gabriel Avila <158302206+Gabri3l-Avi1a@users.noreply.github.com>
2026-04-01 10:00:47 +0200

    Update movement.js

--- js/movement.js
+++ js/movement.js
@@ -1,18 +1,28 @@
 const movableImage = document.getElementById('movableImage');
+const keys = {};
 
 document.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        case 'w': 
-            movableImage.style.top = `${movableImage.offsetTop - 10}px`;
-            break;
-        case 's': 
-            movableImage.style.top = `${movableImage.offsetTop + 10}px`;
-            break;
-        case 'a': 
-            movableImage.style.left = `${movableImage.offsetLeft - 10}px`;
-            break;
-        case 'd': 
-            movableImage.style.left = `${movableImage.offsetLeft + 10}px`;
-            break;
-    }
+    keys[event.key.toLowerCase()] = true;
 });
+
+document.addEventListener('keyup', (event) => {
+    keys[event.key.toLowerCase()] = false;
+});
+
+function moveImage() {
+    if (keys['w']) {
+        movableImage.style.top = `${movableImage.offsetTop - 10}px`;
+    }
+    if (keys['s']) {
+        movableImage.style.top = `${movableImage.offsetTop + 10}px`;
+    }
+    if (keys['a']) {
+        movableImage.style.left = `${movableImage.offsetLeft - 10}px`;
+    }
+    if (keys['d']) {
+        movableImage.style.left = `${movableImage.offsetLeft + 10}px`;
+    }
+    requestAnimationFrame(moveImage);
+}
+
+moveImage();
